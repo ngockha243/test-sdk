672381239b0a01867814210c894d475dfd23d7fd
khann <khann@onechain.game>
2023-08-25 16:58:33 +0700

    sss

---
+++ Samples~/SampleScene/Scripts/Constants.cs
@@ -0,0 +1,13 @@
+public static class Constants
+{
+	// Common
+	public const float PixelsPerUnit = 100.0f;
+
+	// Tags
+	public const string ServicesTag = "Services";
+	public const string ParamsTag   = "Params";
+
+	// Scenes
+	public const string EntryScene = "Scenes/Entry";
+	public const string MainScene = "Scenes/MainScene";
+}
---
+++ Samples~/SampleScene/Scripts/Services/AppsFlyerService.cs
@@ -0,0 +1,16 @@
+using AppsFlyerSDK;
+
+namespace Services
+{
+	public class AppsFlyerService
+	{
+		public AppsFlyerService(string devKey, string appId)
+		{
+			AppsFlyer.initSDK(devKey, appId);
+#if UNITY_DEBUG
+			AppsFlyer.setIsDebug(true);
+#endif
+			AppsFlyer.startSDK();
+		}
+	}
+}
